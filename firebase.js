f1c57285da932a05b87330f46c7d1d976f03b116
camiloj-jime <jimenezcamilo2001@gmail.com>
2026-03-26 17:57:32 -0500

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore, collection, addDoc, getDocs, query, where, setDoc, doc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDt1A5_sJmuXNTMUxAbHdO0awcq8BLMXXE",
+  authDomain: "palaud.firebaseapp.com",
+  projectId: "palaud",
+  storageBucket: "palaud.firebasestorage.app",
+  messagingSenderId: "303175661771",
+  appId: "1:303175661771:web:c38dce685c3696202f834e"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.db = db;
+window.collection = collection;
+window.addDoc = addDoc;
+window.getDocs = getDocs;
+window.query = query;
+window.where = where;
+window.setDoc = setDoc;
+window.doc = doc;
+window.onSnapshot = onSnapshot;
